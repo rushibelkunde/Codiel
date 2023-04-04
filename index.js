4e52fbf1dca400701cc3afa7177493d80ecfd871
rushibelkunde <rushibelkunde18@gmail.com>
2023-04-04 19:27:47 +0530

    added a controller and router for home

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express');
-
 const port = 3000;
-
-
 const app = express()
+
+app.use('/',require('./routes'))
 
 
 
 app.listen(port,(err)=>{
-
     if(err){
         console.log("error")
     }
