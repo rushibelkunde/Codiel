c6a0a1b540df9b5060fdf78a5a57be9d59125b7d
rushibelkunde <rushibelkunde18@gmail.com>
2023-04-04 19:55:34 +0530

    set up the view engine

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
-const port = 3000;
-const app = express()
+const port = 3000;;
+const app = express();
 
-app.use('/',require('./routes'))
+// setup view engine
+app.set('view engine', 'ejs');
+app.set("views" , "./views");
+
+app.use('/',require('./routes'));
+
+
+
 
 
 
 app.listen(port,(err)=>{
     if(err){
-        console.log("error")
+        console.log("error");
     }
-    console.log("server is running")
+    console.log("server is running");
 })
