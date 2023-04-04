e9b84a9348caf64f5117132e7dc7113e60046960
rushibelkunde <rushibelkunde18@gmail.com>
2023-04-04 19:50:49 +0530

    added a controller and router for users

---
+++ controllers/users_controller.js
@@ -0,0 +1,9 @@
+module.exports.profile = (req,res)=>{
+
+    res.send("User Profile")
+
+}
+
+module.exports.posts = (req,res) =>{
+    res.send("User Posts")
+}
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router()
 const homeController = require('../controllers/home_controller')
 
+
 router.get('/', homeController.home)
 
+router.use('/users',require('./users'))
 
 
 module.exports = router;
